d7a0d9ce9356a4494c796ac6e6bc0a998e4683e1
Rafael Loureiro <rafanetmello@gmail.com>
2022-09-19 10:52:48 -0300

    first update drums 1.0

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ document.body.addEventListener('keyup', (event) => {
     playSound(event.code.toLowerCase());
 });
 
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+
+    if (song !== '') {
+        let songArray = song.split('');//transform in Array
+        playComposition(songArray);
+    }
+});
+
+
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
@@ -18,5 +28,16 @@ function playSound(sound) {
         setTimeout(() => {
             keyElement.classList.remove('active');
         }, 100);
+    }
+}
+//transform Array -- play song 
+function playComposition(songArray) {
+    let wait = 0;
+
+    for (let songItem of songArray) {
+        setTimeout(() => {
+            playSound(`key${songItem}`);
+        }, wait);
+        wait += 250;
     }
 }
