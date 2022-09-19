f8a3addc1063030a8174f22b620bf9b9dfe86bea
Rafael Loureiro <rafanetmello@gmail.com>
2022-09-19 10:13:27 -0300

    first update drum leg sound

---
+++ script.js
@@ -0,0 +1,22 @@
+//event = recognize the pressed key
+document.body.addEventListener('keyup', (event) => {
+    playSound(event.code.toLowerCase());
+});
+
+function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+
+    if (audioElement) {
+        audioElement.currentTime = 0;//leg time sound
+        audioElement.play();
+    }
+
+    if (keyElement) {
+        keyElement.classList.add('active');
+
+        setTimeout(() => {
+            keyElement.classList.remove('active');
+        }, 100);
+    }
+}
